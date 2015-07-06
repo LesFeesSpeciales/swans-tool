bd4b23a1ef2726f29d2ee0aa57d2466873e6348d
PiOverFour <dam.pic@free.fr>
2015-07-06 12:19:51 +0200

    Commit and push test

---
+++ blender_fees/__init__.py
@@ -0,0 +1,14 @@
+bl_info = {
+    "name": "Les Fees Speciales",
+    "author": "Les Fees Speciales",
+    "version": (0, 0),
+    "blender": (2, 74, 0),
+    "location": "",
+    "description": "",
+    "warning": "",
+    "wiki_url": "",
+    "category": ""}
+    
+import bpy
+
+
