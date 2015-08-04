869b37502bd8e266c3668ca68b44e1da913713bd
PiOverFour <dam.pic@free.fr>
2015-08-04 18:00:23 +0200

    Added adhoc script for reordering curve vertices

---
+++ various_scripts/reorder_curve_vertices.py
@@ -0,0 +1,43 @@
+import bpy
+import time
+obj = bpy.context.object
+
+verts = []
+edges = []   
+for v in obj.data.vertices:
+    if v.select:
+        verts.append(v)
+        break
+
+print(verts[-1].index)
+while len(verts) != len(obj.data.vertices):
+    print(2, len(verts), len(obj.data.vertices))
+    
+    for e in obj.data.edges:
+        if verts[-1].index in e.vertices and e not in edges:
+            current_edge = e
+            if current_edge.vertices[0] == verts[-1]:
+                v_index = current_edge.vertices[0]
+            else:
+                v_index = current_edge.vertices[1]
+                
+            verts.append(obj.data.vertices[v_index])
+            
+            edges.append(e)
+            break
+print(3)
+edges = [[i, i+1] for i in range(len(verts)-1)]
+
+verts = [v.co for v in verts]
+
+##CREATE NEW MESH
+name = obj.data.name
+new_mesh = bpy.data.meshes.new(obj.data.name + '_Reordered')
+#new_obj = bpy.data.objects.new(obj.name + '_Reordered', new_mesh)
+#bpy.context.scene.objects.link(new_obj)
+new_mesh.from_pydata(verts, edges, [])
+print('lhj')
+obj.data = new_mesh
+obj.data.name = name
+
+print('DONE')
