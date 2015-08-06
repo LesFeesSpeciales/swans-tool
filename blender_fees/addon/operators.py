4f7f15f1440010ab9250fa74424cef1e6033ff16
armabon <armabon@debian>
2015-08-06 21:07:48 +0200

    adding op

--- blender_fees/addon/operators.py
+++ blender_fees/addon/operators.py
@@ -20,7 +20,13 @@ Created by LES FEES SPECIALES
 
 import bpy
 from bpy.props import *
- 
+import os.path            #Files functions of os lib
+import sys  
+from . import files
+from . import gui
+from . import interface
+from . import ressources
+from . import __init__
 
 
 '''<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -47,7 +53,7 @@ class XP(bpy.types.Operator):
     
     def execute(self, context):
         if context.scene.hidec:
-            context.scene.hidec = False
+            context.scene.hid,commandec = False
         else:
             context.scene.hidec = True
             
@@ -69,3 +75,153 @@ class hide(bpy.types.Operator):
             context.scene.hidecreator = True
             
         return {"FINISHED"}
+    
+'''<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+                DIALOG OPERATOR
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>''' 
+class DialogOperator(bpy.types.Operator):
+    bl_idname = "object.dialog_operator"
+    bl_label = "File already exist, overwrite or change version"
+    
+    increase_version = BoolProperty(name="Increase Version")
+    overwrite = BoolProperty(name="Overwrite")
+
+    def execute(self, context):
+        scn = context.scene
+        
+        #If the user want to overwrite
+        if self.overwrite:
+            print("overwrite")
+            p = bpy.context.scene.newF
+            bpy.ops.wm.save_as_mainfile(filepath=p) 
+            
+        #IF user want to add version
+        if self.increase_version:  
+            print("Increase version")
+            
+            ######################
+            path = files.increaseVersion(bpy.context.scene.newF)
+            
+            while os.path.isfile(path):
+                print("FILE ALREADY EXIST ERASE IT")
+                path = files.increaseVersion(path)
+                
+            bpy.ops.wm.save_as_mainfile(filepath=path) 
+            print('save as:'+path)
+
+        files.Update_ListFile(bpy.context.scene.newF)   
+        
+        return {'FINISHED'}
+    
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+'''<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+                FILE OPERATOR
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>''' 
+class file_op(bpy.types.Operator):
+    bl_idname = "scene.file_op"
+    bl_label = "file_op"
+    
+    action = bpy.props.StringProperty() # defining the property
+    
+    def execute(self, context):
+        #CREATING NEW FILES------>
+        if self.action == "NEW": 
+            print("create directories")
+            interface.create_naming(bpy.context,bpy.context,'CREATE',ressources.path,ressources.command)
+            p = bpy.context.scene.newF
+            print("saving file to :"+str(p))  
+            for x in range(0,len(addon_utils.paths())):
+                print(addon_utils.paths()[x]+'/addon/base.blend')
+                if os.path.isfile(addon_utils.paths()[x]+'/addon/base.blend'):
+                    shutil.copyfile(addon_utils.paths()[x]+'/addon/base.blend',p) 
+                    print("new file copied")
+                    break
+                else:
+                    print("copy error")
+            bpy.ops.wm.open_mainfile(filepath = p)
+                
+        #OPENING FILES----------->            
+        elif self.action == "OPEN":
+            print("opening file")
+            file_name =  bpy.context.scene.custom[bpy.context.scene.custom_index].name
+            if len(file_name) > 3: #checking if a file is selected
+                p = files.getPath(bpy.context.scene.newF)+file_name
+                bpy.ops.wm.open_mainfile(filepath = p)
+            else:
+                print('no file selected')
+            
+        #SAVE8AS FILES----------->
+        elif self.action == "SAVE_AS":
+            file_name = bpy.context.scene.newF.split("/")
+            p = bpy.context.scene.newF
+            if os.path.isfile(p):
+                ressources.command.append("File already exist")
+                bpy.ops.object.dialog_operator('INVOKE_DEFAULT') #calls the popup
+            else:
+                ressources.command.append("saving file to :"+str(p))
+                interface.create_naming(bpy.context,bpy.context,'CREATE',ressources.path,ressources.command)
+                print("saving file to :"+str(p))  
+                bpy.ops.wm.save_as_mainfile(filepath=p)    
+                
+            files.Update_ListFile(bpy.context.scene.newF)
+            #__init__.update_naming(self,context)
+        return {"FINISHED"}
+
+'''<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+                OPEN DIR OPERATOR
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>''' 
+class OBJECT_OT_custompath(bpy.types.Operator):
+    bl_idname = "object.custom_path"
+    bl_label = "open"
+    __doc__ = ""   
+    
+    filename_ext = ""
+    filter_glob = StringProperty(default="", options={'HIDDEN'})    
+        
+    #this can be look into the one of the export or import python file.
+    #need to set a path so so we can get the file name and path
+    filepath = StringProperty(name="File Path", description="Filepath used for importing txt files", maxlen= 1024, default= "")
+    files = CollectionProperty(
+        name="File Path",
+        type=bpy.types.OperatorFileListElement,
+        )    
+    def execute(self, context):
+        #set the string path fo the file here.
+        #this is a variable created from the top to start it
+        #bpy.context.scene.newF = self.properties.filepath     
+        
+        print("*************SELECTED FILES ***********")
+        for file in self.files:
+            print(file.name)
+        
+        print("FILEPATH %s"%self.properties.filepath)#display the file name and current path    
+        ressources.Items.append((str(self.properties.filepath),str(self.properties.filepath),""))
+        interface.UpdateEnum(bpy.types.Scene,ressources.Items,'Store',str(self.properties.filepath),str(self.properties.filepath))
+        return {'FINISHED'}
+
+    def draw(self, coitemntext):
+        self.layout.operator('file.select_all_toggle')        
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+        
+
+def register():
+    bpy.utils.register_class(DialogOperator)
+    bpy.utils.register_class(file_op)
+    bpy.utils.register_class(OBJECT_OT_custompath)
+
+def unregister():
+    bpy.utils.unregister_class(DialogOperator)
+    bpy.utils.unregister_class(file_op)
+    bpy.utils.unregister_class(OBJECT_OT_custompath)
